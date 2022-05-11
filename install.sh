e5b98addd23ac3551b203f56adc2499896ec441e
Alex Carruthers <alex.carruthers@shopify.com>
2022-05-11 11:55:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,10 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cp $SCRIPT_DIR/.zshrc ~/.zshrc
 cp $SCRIPT_DIR/.psqlrc ~/.psqlrc
 
-
-
 mkdir -p ~/.config/nvim/
-cp $SCRIPT_DIR/config/nvim ~/.config/nvim/
+cp -r $SCRIPT_DIR/config/nvim/ ~/.config/
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 nvim --noplugin --headless -c 'PlugInstall' -c 'qa'
-
